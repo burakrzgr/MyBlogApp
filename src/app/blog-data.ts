85bb8e0077b42f94a50dab1911bd171feb215100
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-04-16 01:44:34 +0300

    blogs

--- src/app/blog-data.ts
+++ src/app/blog-data.ts
@@ -3,6 +3,7 @@ export interface BlogData {
     id: number;
     author: string;
     entryDate : Date;
+    header : string;
     text? : string;
     imagePath?: string;
   }
--- src/app/blog-service.ts
+++ src/app/blog-service.ts
@@ -1,6 +1,7 @@
 
 import { Injectable } from '@angular/core';
 import { BlogData } from './blog-data';
+import { WrittenBlogs } from './mock-blog-data';
 
 @Injectable({ providedIn: 'root' })
 export class BlogService {
@@ -14,12 +15,3 @@ export class BlogService {
     this.blogs = [];
   }
 }
-
-
-export const WrittenBlogs: BlogData[] = [
-  { id: 1, author: 'Burak',entryDate : new Date(), text : "First Test enntry!!!"},
-  { id: 2, author: 'Narco' ,entryDate : new Date(), text : "First Test enntry!!!"},
-  { id: 3, author: 'Bombasto',entryDate : new Date(), text : "First Test enntry!!!" },
-  { id: 4, author: 'Celeritas',entryDate : new Date(), text : "First Test enntry!!!" },
-  { id: 5, author: 'Magneta' ,entryDate : new Date(), text : "First Test enntry!!!"},
-];
