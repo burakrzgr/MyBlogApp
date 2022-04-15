611c87da8f7ee9d64d7004d74e1e03ebbbfa1be0
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-04-16 00:13:37 +0300

    testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogComponent } from './blog/blog.component';
+import { SplashComponent } from './splash/splash.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/splash', pathMatch: 'full' },
+  { path: 'splash', component: SplashComponent },
   { path: 'blog', component: BlogComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlogComponent } from './blog/blog.component';
+import { SplashComponent } from './splash/splash.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogComponent
+    BlogComponent,
+    SplashComponent
   ],
   imports: [
     BrowserModule,
