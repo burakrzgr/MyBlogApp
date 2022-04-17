33750dae99244ee4571a0d47cf7c8a4026c532c1
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-04-17 23:53:06 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogComponent } from './blog/blog.component';
+import { EditBlogListComponent } from './edit-blog-list/edit-blog-list.component';
+import { EditBlogComponent } from './edit-blog/edit-blog.component';
 import { NewBlogComponent } from './new-blog/new-blog.component';
 import { SplashComponent } from './splash/splash.component';
 
@@ -9,6 +11,8 @@ const routes: Routes = [
   { path: 'splash', component: SplashComponent },
   { path: 'blog', component: BlogComponent },
   { path: 'new', component: NewBlogComponent },
+  { path: 'myblogs', component: EditBlogListComponent },
+  { path: 'myblogs/:id', component: EditBlogComponent },
 ];
 
 @NgModule({
