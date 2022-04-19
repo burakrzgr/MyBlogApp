62528f86d85f136a7104b06fb09f5d800c019398
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-04-19 21:18:06 +0300

    edit

--- src/app/edit-blog-list/edit-blog-list.component.ts
+++ src/app/edit-blog-list/edit-blog-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BlogData } from '../blog-data';
+import { BlogService } from '../blog-service';
 
 @Component({
   selector: 'app-edit-blog-list',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditBlogListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public blogService: BlogService,private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  goToEdit(blog:BlogData){
+    this.router.navigateByUrl('/myblogs/'+blog.id);
+  }
 }
