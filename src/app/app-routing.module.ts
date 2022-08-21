2558c553520b06efae3fc9a1f9929ce7af9c64ab
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-08-21 23:55:05 +0300

    permission

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { BlogComponent } from './blog/blog.component';
 import { EditBlogListComponent } from './edit-blog-list/edit-blog-list.component';
 import { EditBlogComponent } from './edit-blog/edit-blog.component';
+import { NoPermissionComponent } from './error/no-permission/no-permission.component';
 import { NotFoundComponent } from './error/not-found/not-found.component';
 import { NewBlogComponent } from './new-blog/new-blog.component';
 import { SplashComponent } from './splash/splash.component';
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'myblogs', component: EditBlogListComponent },
   { path: 'myblogs/:id', component: EditBlogComponent },
   { path: 'error/404', component: NotFoundComponent },
+  { path: 'error/403', component: NoPermissionComponent },
 ];
 
 @NgModule({
