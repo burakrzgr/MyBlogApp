2d8813be5665e44c72602dbae6d7239f976f548e
Burak Rüzgar <62022339+burakrzgr@users.noreply.github.com>
2022-08-21 22:27:10 +0300

    edts

--- src/app/comment-data.ts
+++ src/app/comment-data.ts
@@ -2,5 +2,5 @@ export interface CommentData{
     enterNew?:boolean;
     loading:boolean;
     displayCount?:number;
-    list? : {writer:string,text:string,submitDate?:Date}[] 
+    list? : {writer:string,text:string,submitDate?:Date,subComment?:CommentData}[] 
    }
